d2724f4e9b6be768b4870af8b1519dcb236b15dd
Jose Pinto <pintojose.benedicto2@gmail.com>
2019-11-05 21:43:54 -0700

    Added Flask Blueprints

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,39 +1,22 @@
 import os
-import datetime
-import sys
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
-# instantiate the app
-app = Flask(__name__)
+from project.api.users import users_blueprint
 
 # Instanciate database
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
-# model
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-
-# set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
-
-print(app.config, file=sys.stderr)
-
-
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+def create_app(script_info=None):
+    # Instanciate the app
+    app = Flask(__name__)
+    # Set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+    # Set up extensions
+    db.init_app(app)
+    # Register blueprints
+    app.register_blueprint(users_blueprint)
+    # Shell contextfor flask cli
+    app.shell_context_processor({'app': app, 'db': db})
+    return app
