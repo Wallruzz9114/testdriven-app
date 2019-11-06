a48f96077e3123f3fb1e14c86666c76fa4b7c005
Jose Pinto <pintojose.benedicto2@gmail.com>
2019-11-05 19:51:23 -0700

    Set up postgres

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,10 +1,34 @@
+import os
+import datetime
+import sys
 from flask import Flask, jsonify
-# instantiate the app
+from flask_sqlalchemy import SQLAlchemy
 
+# instantiate the app
 app = Flask(__name__)
 
+# Instanciate database
+db = SQLAlchemy(app)
+
+
+# model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+print(app.config, file=sys.stderr)
 
 
 @app.route('/users/ping', methods=['GET'])
